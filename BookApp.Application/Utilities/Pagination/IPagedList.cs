edbeb34aa4661c3dd132f6c8bddd316fe8a95707
successJohn <successojohn@gmail.com>
2023-10-13 10:27:35 +0100

    Pagination

---
+++ BookApp.Application/Utilities/Pagination/IPagedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp.Application.Utilities.Pagination
+{
+    public interface IPagedList
+    {
+        int PageCount { get; }
+
+        int TotalItemCount { get; }
+
+        int PageNumber { get; }
+
+        int PageSize { get; }
+
+        bool HasPreviousPage { get; }
+
+        bool HasNextPage { get; }
+    }
+    public interface IPagedList<out T> : IPagedList, IEnumerable<T>
+    {
+        T this[int index] { get; }
+
+        int Count { get; }
+
+        IEnumerable<T> Items { get; }
+
+        IPagedList GetMetaData();
+    }
+}
