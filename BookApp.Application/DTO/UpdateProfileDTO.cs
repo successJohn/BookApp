eb02415e29d575d4611329d9e454b07c99e62eae
successJohn <successojohn@gmail.com>
2024-04-03 23:15:53 +0100

    update profile

---
+++ BookApp.Application/DTO/UpdateProfileDTO.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp.Application.DTO
+{
+    public class UpdateProfileDTO
+    {
+        public IFormFile profilePicture { get; set; }
+    }
+}
